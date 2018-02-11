206219f5df9e48dca895aca1d4211ad52827a04c
Ragavendira <ragavendira.rajathirumalai>
2018-02-11 21:33:47 +0530

    this checking

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		Main.displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting() {
+		// this is checking 
+		System.out.println("Hello happy sales people");
+		System.out.println("This app shows sale data");
+		System.out.println("-------------------------");
+		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+	}
+}
